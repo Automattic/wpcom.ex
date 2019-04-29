5c23f5377816d900fadc1fc5ca0b939dfbcfe677
Hew <github@mhs.fastmail.com>
2019-04-29 16:15:41 -0400

    Add Hex.pm package information

--- mix.exs
+++ mix.exs
@@ -7,21 +7,34 @@ defmodule Wpcom.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "wpcom.ex",
+      source_url: "https://github.com/Automattic/wpcom.ex"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [extra_applications: [:logger]]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:jason, "~> 1.1"},
       {:httpoison, "~> 1.5"},
       {:retry, "~> 0.11"}
+    ]
+  end
+
+  defp description() do
+    "wpcom.ex is the official Elixir library for the WordPress.com REST API."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Automattic/wpcom.ex"}
     ]
   end
 end

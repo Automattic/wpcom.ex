eebafbddf215306a67050f1d1c5c73e2c8143903
Hew <github@mhs.fastmail.com>
2019-04-29 11:26:28 -0400

    Smarten exponential backoff logic in cast

--- lib/wpcom/cast.ex
+++ lib/wpcom/cast.ex
@@ -2,22 +2,39 @@ defmodule Wpcom.Cast do
   @moduledoc """
   Asynchronous HTTP request functions with infinite exponential retry
   to the WordPress.com REST API.
+
+  This cast functionality makes zero guarentees about order of delivery.
+  It is highly suggested that all operations be idempotent.
+
+  We rely on WP.com API's incredible uptime and expected recovery times
+  in failure cases in our assumptions below.
   """
+
   use Retry
   require Logger
 
-  @doc "Performs asynchronous POST request to the WP.com API"
-  @spec post(String.t(), String.t(), [{String.t(), String.t()}]) :: {:ok, pid()}
+  @minute_in_ms 60_000
+  @max_backoff 60 * @minute_in_ms
+
+  @doc """
+  Performs asynchronous POST request to the WP.com API.
+
+  The request is fired off in an unsupervised task that does
+  an exponential backoff up to the the backoff cap and then
+  continues retrying (indefinitely) at that pace.
+  """
+  @spec post(String.t(), %{} | String.t(), [{String.t(), String.t()}]) :: {:ok, pid()}
   def post(path, body, headers \\ []) do
     Task.start(fn ->
-      retry_while with: exponential_backoff(2000) |> randomize() do
+      retry_while with: exponential_backoff(2000) |> cap(@max_backoff) |> randomize() do
         Wpcom.Req.request(:post, path, headers, body)
         |> case do
           {:error, resp} ->
-            Logger.warn("http req cast failed!", resp: resp)
+            Logger.warn("#{inspect(self())} http req cast failed!", resp: resp)
             {:cont, resp}
 
           {_, resp} ->
+            Logger.debug("http req cast succeeded", resp: resp)
             {:halt, resp}
         end
       end

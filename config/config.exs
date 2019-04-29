1599661f69b66fa9ef2cc35be8ec48cab3a62c6e
Hew <github@mhs.fastmail.com>
2019-04-29 11:27:04 -0400

    Switch logging to warn level

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
+config :logger, level: :warn
+
 config :wpcom, restV1: "https://public-api.wordpress.com/rest/v1"
 config :wpcom, restV11: "https://public-api.wordpress.com/rest/v1.1"
 config :wpcom, wpV2: "https://public-api.wordpress.com/wp/v2"

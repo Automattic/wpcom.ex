33045b4ea72c1417b486c0504bb511c4a565643d
Matt Sutton <2006764+mhsdef@users.noreply.github.com>
2023-02-14 00:58:39 -0500

    Add CI (#52)
    * Add CI
    
    * Adjust
    
    * Fine, be that way

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wpcom.MixProject do
   def project do
     [
       app: :wpcom,
-      version: "0.8.0",
+      version: "1.0.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
